2f005abfa8d18d5ed729d7624e3d5c2be2c4a042
Nelson Peterson <nelsonpeterson@Nelsons-MacBook-Air.local>
2023-11-03 12:46:26 -0600

    puppylist added to function app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import { useState } from 'react'
+import { puppyList } from './data'
 import './App.css'
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList)
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
+    </div>
   )
 }
 
